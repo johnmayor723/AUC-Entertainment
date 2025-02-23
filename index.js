b0aae4a64eb667b4a39c907c0682b6caa44f488c
may <may@xymail.com>
2025-02-23 07:36:36 +0100

    bvc

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const bodyParser = require('body-parser');
 const Stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const methodOverride = require('method-override');
 const expressLayouts = require('express-ejs-layouts');
-
+const flash = require('connect-flash');
+const MongoStore = require('connect-mongo');
 const app = express();
 const DB_URL = "mongodb+srv://admin:majoje1582@cluster0.cqudxbr.mongodb.net/?retryWrites=true&w=majority"
 
@@ -31,6 +32,41 @@ app.set('layout', 'layout');*/
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
+// Setting app middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(session({
+  secret: 'mysupersecret',
+  resave: false,
+  saveUninitialized: false,
+  store: MongoStore.create({ mongoUrl: DB_URL }),
+  cookie: { secure: false }
+}));
+app.use(flash());
+
+// Middleware to pass session data and flash message to views
+app.use((req, res, next) => {
+  res.locals.session = req.session;
+  next();
+});
+
+// Initialize cart in session
+app.use((req, res, next) => {
+    if (!req.session.cart) {
+        req.session.cart = {
+            items: [],
+            totalQty: 0,
+            totalAmount: 0
+        };
+    }
+    next();
+});
+
+app.use((req, res, next) => {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    next();
+});
+
 
 // Routers
 const indexRouter = require('./routes/index');
