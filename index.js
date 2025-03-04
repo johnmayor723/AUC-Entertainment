717cad95c6c8f45a0b904cd90c91bebda9c275fc
may <may@xymail.com>
2025-03-04 12:44:49 +0100

    nbvv

--- index.js
+++ index.js
@@ -81,7 +81,7 @@ app.use('/payment', PaymentRouter);
 app.use('/cart', CartRouter);
 
 // Start server
-const PORT = 5000;
+const PORT = 6000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
