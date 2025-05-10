5cd86787aa1ca192b6c31eda0df820920e68b966
johnmayor723 <johnmayor723@yahoo.com>
2025-05-10 08:08:09 +0100

    Update index.js

--- index.js
+++ index.js
@@ -81,7 +81,7 @@ app.use('/payment', PaymentRouter);
 app.use('/cart', CartRouter);
 
 // Start server
-const PORT = 3000;
+const PORT = 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
